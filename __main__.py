60d0b5fc4c71630f704cf96844145dde3338733e
Droidel14799 <lukas14799@aol.com>
2025-03-08 16:27:43 +0100

    Basic webhook functionality

--- __main__.py
+++ __main__.py
@@ -1,2 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/Webhook", methods=["POST"])
+
+def hook():
+    data = request.json
+    print("Received webhook data:", data)
+    return jsonify({'message': 'Webhook received successfully'}), 200
+ 
+
+if "__name__"=="__main__":
+    app.run()
