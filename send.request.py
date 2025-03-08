2a8ac9f89ea180804626abeba7de1643fbe9860b
Droidel14799 <lukas14799@aol.com>
2025-03-08 17:59:13 +0100

    addings json data to the POST request

--- send.request.py
+++ send.request.py
@@ -1,7 +1,22 @@
 import requests
+import json
 
+#URL to address
 
-url = "http://127.0.0.1:5000"
+url = "http://127.0.0.1:5000/Webhook"
 
-r = requests.post(url)
+# header to declare the content-Type
+
+headers = {"Content-Type":"application/json"}
+
+# json format data
+
+data = {
+"Id": 12345,
+"Customer": "David",
+"Quantity": 1,
+"Price": 19.99
+}
+
+r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r)
