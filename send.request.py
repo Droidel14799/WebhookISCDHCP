18512d0accc239be4d87178b44beb04f4c45c826
Droidel14799 <lukas14799@aol.com>
2025-03-08 19:35:58 +0100

    funny things

--- send.request.py
+++ send.request.py
@@ -1,6 +1,27 @@
 import requests
 import json
 
+varprice    = 16.49
+varQuantity = 2
+varCustomer = "David"
+varId       = 44444
+
+#URL to address
+url = "http://127.0.0.1:5000/Webhook"
+
+# header to declare the content-Type
+headers = {"Content-Type":"application/json"}
+
+# json format data
+
+dataload = {
+"Id": varId,
+"Customer": varCustomer,
+"Quantity": varQuantity,
+"Price": varprice
+}
+
+
 class content:
     def __init__(self, data, url, headers):
         self.data = data
@@ -9,22 +30,5 @@ class content:
         r = requests.post(self.url, headers=self.headers, data=json.dumps(self.data))
         print(r)
 
-
-#URL to address
-
-url = "http://127.0.0.1:5000/Webhook"
-
-# header to declare the content-Type
-
-headers = {"Content-Type":"application/json"}
-
-# json format data
-
-dataload = {
-"Id": 12345,
-"Customer": "David",
-"Quantity": 1,
-"Price": 19.99
-}
 
 content(dataload, url, headers)
